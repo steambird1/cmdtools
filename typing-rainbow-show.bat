43ba809f67e3804fcbbce5507db7dd3cb561c1b1
steambird1 <steambird@163.com>
2021-01-22 21:08:57 +0800

    'typing_rainbow' showing

---
+++ typing-rainbow-show.bat
@@ -0,0 +1,9 @@
+@echo off
+title Rainbow-typing showing
+:show
+cls
+echo Alphabets
+echo.
+cmdt -e typing_rainbow ABCDEFGHIJKLMNOPQRSTUVWXYZ
+cmdt -e typing_rainbow abcdefghijklmnopqrstuvwxyz
+goto show
