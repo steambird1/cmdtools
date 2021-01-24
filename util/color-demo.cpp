e5a329714d065821408aa206e5789d39f5bd748f
steambird1 <steambird@163.com>
2021-01-24 08:45:29 +0800

    color demo remix

--- util/color-demo.cpp
+++ util/color-demo.cpp
@@ -8,7 +8,7 @@ int main() {
 	setColor("07");
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 15; j++) {
-			if (i==j) continue;
+			//if (i==j) continue;
 			string c = "";
 			c += cl[i];
 			c += cl[j];
